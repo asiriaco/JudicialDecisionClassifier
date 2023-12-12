445f27bd9a726db51bd50f00c7c8a0f0c7a29279
Arthur Siríaco <tutusiriaco@gmail.com>
2023-12-12 17:08:03 -0300

    Updated consolidated dataset and preprocessor

--- preprocessor.py
+++ preprocessor.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 #Global variables
 PATTERN = r'\d+¦'
-SAMPLES_PER_CLASS = 250
+SAMPLES_PER_CLASS = 1250
 
 # Labels dictionary
 predition_mapper = {
@@ -28,10 +28,12 @@ for file in files:
         raw_text = f.read()    
         label = int(file[0])
         sentences = re.split(PATTERN, raw_text)[1:]
-        samples[predition_mapper[label]] = list(map(lambda x: (label ,x.replace("||BDC_DECISION_SEP||", " ").replace("\n", " ").lower()), sentences))[:SAMPLES_PER_CLASS]
+        #Extracting the same number of random samples from each class
+        samples[predition_mapper[label]] = random.sample(list(map(lambda x: (label ,x.replace("||BDC_DECISION_SEP||", " ").replace(";", "").replace("\n", " ").lower()), sentences)), SAMPLES_PER_CLASS)
 
 consolidated_dataset = sum(samples.values(), [])
 random.shuffle(consolidated_dataset)
+print(len(consolidated_dataset))
 
 #Exporting consolidated dataset to csv
 df = pd.DataFrame(consolidated_dataset, columns=['label', 'text'])
