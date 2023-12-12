12fbc3cbdcf09ae96596fa961a83cfa9d8018c7d
Arthur Siríaco <tutusiriaco@gmail.com>
2023-12-12 00:08:25 -0300

    Preprocessing
    Developed script that preprocesses dataset and exports the consolidated dataset to a CSV file.

---
+++ preprocessor.py
@@ -0,0 +1,38 @@
+import os
+import re
+import random
+import pandas as pd
+
+#Global variables
+PATTERN = r'\d+¦'
+SAMPLES_PER_CLASS = 250
+
+# Labels dictionary
+predition_mapper = {
+   0: 'undefined', #not identified yet
+   1: 'no_winner', #extinction or settlement 
+   2: 'author',    #active party winning
+   3: 'defendant', #passive party winning
+}
+
+samples = {}    
+dataset_path = os.path.join(os.getcwd(), 'dataset')
+files = os.listdir(dataset_path)
+raw_text = ""
+print(files)
+
+#Preprocessing raw data and creating a consolidated dataset
+for file in files:
+    raw_text = ""
+    with open(os.path.join(dataset_path, file), 'r') as f:
+        raw_text = f.read()    
+        label = int(file[0])
+        sentences = re.split(PATTERN, raw_text)[1:]
+        samples[predition_mapper[label]] = list(map(lambda x: (label ,x.replace("||BDC_DECISION_SEP||", " ").replace("\n", " ").lower()), sentences))[:SAMPLES_PER_CLASS]
+
+consolidated_dataset = sum(samples.values(), [])
+random.shuffle(consolidated_dataset)
+
+#Exporting consolidated dataset to csv
+df = pd.DataFrame(consolidated_dataset, columns=['label', 'text'])
+df.to_csv('consolidated_dataset.csv', index=False)
